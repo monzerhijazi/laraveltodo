1588c84d56b96ad037e9cc939a89af5308f1f9de
Monzer Hijazi <monzerhijazi@gmail.com>
2019-05-09 10:52:52 -0400

    wrapping up authentication

--- database/migrations/2019_05_09_122209_add_api_tokens_to_user_table.php
+++ database/migrations/2019_05_09_122209_add_api_tokens_to_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddRolesToUsersTable extends Migration
+class AddApiTokensToUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,10 @@ class AddRolesToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('student');
+            $table->string('api_token', 80)->after('password')
+                        ->unique()
+                        ->nullable()
+                        ->default(null);
         });
     }
 
@@ -26,7 +29,7 @@ class AddRolesToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role');
+            $table->dropColumn('api_token');
         });
     }
 }
